eabf3a9524420111f4dd32efd658c57c559b6fd1
Jacobo Asorey Barreiro <jacobo@jasorey.physics.illinois.edu>
2016-02-18 10:05:51 -0600

    Adding library

---
+++ lib_write_debug.py
@@ -0,0 +1,112 @@
+import json
+import os
+import pdb
+import sys
+import numpy as np
+import itertools as it
+from scipy.interpolate import splev,splint
+from collections import defaultdict
+import tables
+
+
+def h5setattr(obj, attr, val):
+    """Set attribute in JSON format."""
+
+    if isinstance(val, np.ndarray):
+        kind = val.dtype.kind
+        if kind == 'i':
+            val = map(int, val)
+        elif kind == 'f':
+            val = map(float, val)
+        else:
+            raise ValueError('Not implemented')
+
+    val = json.dumps(val)
+ 
+    setattr(getattr(obj, 'attrs'), attr, val)
+
+
+class observ:
+      
+    def parametrizations(self,obs):
+        parametrization_name=obs['name_par']['method']
+        if (parametrization_name == '3Dfid'):
+            print(parametrization_name)
+        if (parametrization_name == '2D'):
+            print(parametrization_name)
+            observ.nz = 4    
+        if (parametrization_name == '3Dobs'):
+            print(parametrization_name)
+
+
+class lib_read:
+    /* Reads the files with the parametrization and correlations/covariances*/
+
+    def read_meta(self,out_path,objdict):
+        self._fb = tables.openFile(out_path, 'w')
+        self._fb.createGroup('/', 'meta')
+        obsobj=observ()
+        obsobj.parametrizations(objdict)
+        print(obsobj.nz)
+        meta = self._fb.createArray('/meta', 'meta', np.zeros(0))
+
+
+    def read_meta_parametrization(self, observ):
+        """Add meta data for each parametrization to HDF5 files."""
+        
+        for i, pop_name in enumerate(wl.pop['meta']['pop']):
+            group_pop = '/meta/'+parametrization_name
+            meta_parametrization = self._fb.createArray(group_pop, 'meta', np.zeros(0),createparents=True)
+       
+            
+            pop = wl.pop[pop_name]
+            h5setattr(meta_parametrization, 'z_mean', parametrization['z_mean'])
+            h5setattr(meta_parametrization, 'z_width', parametrization['z_width'])
+
+
+            
+class lib_write:
+    
+
+    def add_meta(self,out_path,objdict):
+        self._fb = tables.openFile(out_path, 'w')
+        self._fb.createGroup('/', 'meta')
+        obsobj=observ()
+        obsobj.parametrizations(objdict)
+        print(obsobj.nz)
+        meta = self._fb.createArray('/meta', 'meta', np.zeros(0))
+
+
+    def add_meta_parametrization(self, observ):
+        """Add meta data for each parametrization to HDF5 files."""
+        
+        for i, pop_name in enumerate(wl.pop['meta']['pop']):
+            group_pop = '/meta/'+parametrization_name
+            meta_parametrization = self._fb.createArray(group_pop, 'meta', np.zeros(0),createparents=True)
+       
+            
+            pop = wl.pop[pop_name]
+            h5setattr(meta_parametrization, 'z_mean', parametrization['z_mean'])
+            h5setattr(meta_parametrization, 'z_width', parametrization['z_width'])
+
+
+
+def main():
+    
+    file_name="trial0.h5"
+    #add_meta(file_name)   
+    name_parametrization = '2D'
+    objdict = {'name_par':{'method':name_parametrization,'nz':4}}
+    #objdict['name_par'] = name_parametrization
+    print(objdict['name_par'])   
+    print(objdict['name_par']['method'])
+    
+    x=lib_write()
+    x.add_meta(file_name,objdict)
+    
+
+    return
+    
+if __name__ == '__main__':
+    main()
+
